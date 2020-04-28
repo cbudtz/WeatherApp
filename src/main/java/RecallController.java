848da9223cf4c8ee4b24b30b9bf02d273ec82b5a
Christian Budtz <chrbudtz@gmail.com>
2020-04-28 23:59:41 +0200

    Now with time

--- src/main/java/RecallController.java
+++ src/main/java/RecallController.java
@@ -2,6 +2,7 @@ import data.WeatherDAO;
 import data.WeatherDAOSqlImpl;
 import data.WeatherData;
 import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 
 import java.sql.Timestamp;
@@ -9,17 +10,17 @@ import java.util.List;
 
 public class RecallController {
     public TextArea recallArea;
+    public DatePicker dateStart;
     private WeatherDAO weatherDao = new WeatherDAOSqlImpl();
 
     public void recallData(ActionEvent actionEvent) {
-        Timestamp from = new Timestamp(System.currentTimeMillis()-5*60*1000);
+        Timestamp from = Timestamp.valueOf(dateStart.getValue().atStartOfDay());
         List<WeatherData> load = weatherDao.load(from, new Timestamp(System.currentTimeMillis()));
+        String text = "";
         for (WeatherData data:load){
-            String text = recallArea.getText();
             text += "ID: " + data.getId() + "WindSpeed: " + data.getWindSpeed() + "\r\n";
-            recallArea.setText(text);
         }
-
+        recallArea.setText(text);
 
     }
 }
